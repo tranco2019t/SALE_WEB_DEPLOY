9f190dc9e4d55350192edd3a813e25ff36f37839
tranco2019t <tranco2016t@gmail.com>
2026-05-26 14:17:26 +0700

    fix CORS and API URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -51,7 +51,9 @@ app.add_middleware(
         "http://[::1]:5500",
         "http://localhost:5500",
         "http://127.0.0.1:8000",
-        "http://localhost:8000"
+        "http://localhost:8000",
+        "https://sale-web-frontend.onrender.com",
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
